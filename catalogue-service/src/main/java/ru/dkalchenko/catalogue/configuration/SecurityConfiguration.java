3117ba89705f4657ffc47f994040c7f9a4ba122a
denfort50 <denfort50@yandex.ru>
2024-03-28 09:00:46 +0300

    Spring Security upgraded by using Keycloak

--- catalogue-service/src/main/java/ru/dkalchenko/catalogue/configuration/SecurityConfiguration.java
+++ catalogue-service/src/main/java/ru/dkalchenko/catalogue/configuration/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package ru.dkalchenko.catalogue.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,12 +13,22 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeHttpRequests(authorizedHttpRequests ->
-                        authorizedHttpRequests.requestMatchers("/catalogue-api/**")
-                                .hasRole("SERVICE"))
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizedHttpRequests -> authorizedHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/catalogue-api/products")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.PATCH, "/catalogue-api/products/{productId:\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.DELETE, "/catalogue-api/products/{productId:\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.GET)
+                        .hasAuthority("SCOPE_view_catalogue")
+                        .anyRequest().denyAll())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults()))
+                .oauth2Client(Customizer.withDefaults())
                 .build();
     }
 }
--- manager-application/src/main/java/ru/dkalchenko/manager/configuration/SpringConfiguration.java
+++ manager-application/src/main/java/ru/dkalchenko/manager/configuration/SpringConfiguration.java
@@ -3,19 +3,26 @@ package ru.dkalchenko.manager.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
+import ru.dkalchenko.manager.security.OAuthClientHttpRequestInterceptor;
 
 @Configuration
 public class SpringConfiguration {
 
     @Bean
     public RestClient restClient(@Value("${catalogue.service.url:http://localhost:8081}") String catalogueServiceUrl,
-            @Value("${catalogue.service.username:}") String username,
-            @Value("${catalogue.service.password:}") String password) {
+                                 ClientRegistrationRepository clientRegistrationRepository,
+                                 OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository,
+                                 @Value("${catalogue.service.registration-id:keycloak}") String registrationId) {
         return RestClient.builder()
                 .baseUrl(catalogueServiceUrl)
-                .requestInterceptor(new BasicAuthenticationInterceptor(username, password))
+                .requestInterceptor(new OAuthClientHttpRequestInterceptor(
+                        new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                oAuth2AuthorizedClientRepository), registrationId
+                ))
                 .build();
     }
 }
